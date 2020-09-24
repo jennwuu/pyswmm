92e6a455b72f542cfc69085980d0f396a2a63762
Jenn Wu <jwu@emnet.net>
2020-09-23 20:49:23 -0400

    add warning for swmm5 which will be deprecated in the next release

--- pyswmm/swmm5.py
+++ pyswmm/swmm5.py
@@ -25,16 +25,22 @@ import six
 
 # Local imports
 from pyswmm.lib import DLL_SELECTION
+from pyswmm.error import SWMM5FutureWarning
 import pyswmm.toolkitapi as tka
 
 # Local variables
 SWMM_VER_51011 = '5.1.14'
 
 
+warnings.warn('the swmm5 module is deprecated in next release with swmm-toolkit module update',
+              SWMM5FutureWarning)
+
+
 class SWMMException(Exception):
     """Custom exception class for SWMM errors."""
 
     def __init__(self, error_code, error_message):
+
         self.warning = False
         self.args = (error_code, )
         self.message = error_message
@@ -120,6 +126,10 @@ class PySWMM(object):
         :param str swmm_lib_path: SWMM library path (default None).
 
         """
+
+        warnings.warn('the swmm5 module is deprecated in next release with swmm-toolkit module update',
+                      SWMM5FutureWarning)
+        
         self.fileLoaded = False
         self.inpfile = inpfile
         self.rptfile = rptfile
